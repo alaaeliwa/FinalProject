6f3e517d4d340605d32482d97674fd2bbbc678e9
alaaeliwa <alaahanieliwa@gmail.com>
2023-08-31 14:42:39 +0300

    TODO 2 Define Course Class and define constructor

--- FinalProject.py
+++ FinalProject.py
@@ -3,3 +3,16 @@
 Name : Alaa' Hani Eliwa
 Delivery Date :31-8-2023
 """
+
+# TODO 2 Define Course Class and define constructor with
+# course_id (generated using uuid4) ,
+# course name (user_input) and
+# course mark (user_input)
+
+import uuid
+
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
